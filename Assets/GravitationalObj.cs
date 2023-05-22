1650867a37a12a4562c680b43bbb60a1d3df6687
Kaylie Sampson <kksamps2@gmail.com>
2023-05-22 02:10:11 -0400

    finished earth and moon simulation

--- Assets/GravitationalObj.cs
+++ Assets/GravitationalObj.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class GravitationalObj : MonoBehaviour
 {
     public Rigidbody rb;
+    public float velocityX;
+    public float velocityY;
 
-    const float G = 667.4f;
+    const float G = 6.67430f;
 
     // scales for distances and velocities
-    public float scaleDistance = 1000f;
-    public float scaleVelocity = 0.1f;
+    float scaleDistance = 3.85f;   // Adjusted scale for distance
+    float scaleVelocity = 0.0108f;  // Adjusted scale for velocity
 
     // list of all objects in the scene
     public static List<GravitationalObj> Objs;
 
     void FixedUpdate()
     {
+        Time.fixedDeltaTime = 0.001f;
         // loop through all our objects and call our Attract method
         foreach (GravitationalObj obj in Objs)
         {
@@ -24,7 +27,7 @@ public class GravitationalObj : MonoBehaviour
             {
                 Attract(obj);
             }
-        };
+        }
     }
 
     void OnEnable()
@@ -33,7 +36,10 @@ public class GravitationalObj : MonoBehaviour
         {
             Objs = new List<GravitationalObj>();
         }
-        Objs.Insert(0, this); // Insert the moon object at the beginning of the list
+        Objs.Add(this); // Add the Moon object to the end of the list
+
+        // Adjust the initial position of the Moon slightly
+        rb.position += new Vector3(0f, 0.01f, 0f); // Move the Moon slightly above the Earth's plane
     }
 
     void OnDisable()
@@ -46,34 +52,40 @@ public class GravitationalObj : MonoBehaviour
     {
         Rigidbody rbToAttract = objToAttract.rb;
 
-        // find direction and distance of objects
+        // Find direction and distance of objects
         Vector3 direction = rb.position - rbToAttract.position;
         float distance = direction.magnitude;
 
-        // if objects are in the exact same position, return
+        // If objects are in the exact same position, return
         if (distance == 0f)
         {
             return;
         }
 
-        // scale the distance and velocity
+        // Scale the distance and direction
         float scaledDistance = distance / scaleDistance;
-        Vector3 scaledDirection = direction.normalized * scaledDistance;
+        Vector3 scaledDirection = direction.normalized;
 
-        // Using Newton's law to calculate the force on the object
+        // Calculate the force magnitude
         float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(scaledDistance, 2);
-        Vector3 force = scaledDirection.normalized * forceMagnitude;
+        Vector3 force = scaledDirection * forceMagnitude;
 
         rbToAttract.AddForce(force);
 
         // Calculate the velocity required for orbital motion
-        Vector3 velocity = new Vector3(0f, 0f, 0f); // Earth's velocity remains 0
-        if (gameObject.name == "Moon")
-        {
-            velocity.x = 0.1008f * scaleVelocity; // Set the Moon's scaled velocity
-            velocity.y = 0.1008f * scaleVelocity;
-        }
-        velocity += force / rbToAttract.mass * Time.fixedDeltaTime;
+        Vector3 velocity = new Vector3(velocityX, velocityY, 0f);
+        velocity *= scaleVelocity;
+
+        // Adjust the velocity based on the orbital motion
+        Vector3 relativePosition = rbToAttract.position - rb.position;
+        Vector3 relativeVelocity = rbToAttract.velocity - rb.velocity;
+        float orbitalSpeed = Mathf.Sqrt(G * rb.mass / distance); // Calculate orbital speed
+
+        // Get the tangential velocity direction perpendicular to the relative position
+        Vector3 tangent = new Vector3(-relativePosition.y, relativePosition.x, 0f).normalized;
+
+        // Set the final velocity
+        velocity += tangent * orbitalSpeed;
         rbToAttract.velocity = velocity;
     }
 }
