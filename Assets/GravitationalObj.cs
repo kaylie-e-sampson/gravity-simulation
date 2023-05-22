0a81b81660194bb074e4440509655d2f6ecb7e6c
Kaylie Sampson <kksamps2@gmail.com>
2023-05-21 21:57:41 -0400

    finished adding gravity to earth

--- Assets/GravitationalObj.cs
+++ Assets/GravitationalObj.cs
@@ -6,17 +6,38 @@ public class GravitationalObj : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public float velocityX;
+    public float velocityY;
+
+    const float G = 667.4f;
+
+    // list of all objects in the scene
+    public static List<GravitationalObj> Objs;
+
     void FixedUpdate()
     {
         // loop through all our objects and call our Attract method
-        GravitationalObj[] objs = FindObjectsOfType<GravitationalObj>();
-        foreach (GravitationalObj obj in objs)
+        foreach (GravitationalObj obj in Objs)
         {
             if (obj != this)
             {
                 Attract(obj);
             }   
         };
+    }
+
+    void OnEnable()
+    {
+        if (Objs == null)
+        {
+            Objs = new List<GravitationalObj>();
+        }
+        Objs.Add(this);
+    }
+
+    void OnDisable()
+    {
+        Objs.Remove(this);
     }
 
     // deals with the gravitational force between 2 objects
@@ -28,8 +49,14 @@ public class GravitationalObj : MonoBehaviour
         Vector3 direction = rb.position - rbToAttract.position;
         float distance = direction.magnitude;
 
+        // if objects are in the exact same position, return
+        if (distance == 0f)
+        {
+            return;
+        }
+
         // using newton's law to calculate the force on the object
-        float forceMagnitude = (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
         Vector3 force = direction.normalized * forceMagnitude;
 
         rbToAttract.AddForce(force);
